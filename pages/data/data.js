896b5959c426c5a62aa878021df1451d2d9c3eae
PGDAO <liurenjun@me.com>
2019-09-06 23:46:14 +0800

    提交信息

---
+++ pages/data/data.js
@@ -0,0 +1,8 @@
+var data = [
+  {
+    "id":0,
+    "img":"http://10.103.11.4/data/attachment/portal/201909/03/110101qs343214o71szfss.jpg.thumb.jpg",
+    "title":"南通中支：真诚服务客户、坚守职业道德",
+    "content":"8月27日，海安淘宝卖家网上刊登了一份表扬信，信中对南通中支理赔部人伤岗顾嘉优质..."
+  }
+]
---
+++ pages/index/index.js
@@ -0,0 +1,41 @@
+Page({
+  data:{
+    indicatorDots:true,
+    autoplay:true,
+    interval:2000,
+    circular:true
+  },
+  onLoad(query) {
+    // 页面加载
+    console.info(`Page onLoad with query: ${JSON.stringify(query)}`);
+  },
+  onReady() {
+    // 页面加载完成
+  },
+  onShow() {
+    // 页面显示
+  },
+  onHide() {
+    // 页面隐藏
+  },
+  onUnload() {
+    // 页面被关闭
+  },
+  onTitleClick() {
+    // 标题被点击
+  },
+  onPullDownRefresh() {
+    // 页面被下拉
+  },
+  onReachBottom() {
+    // 页面被拉到底部
+  },
+  onShareAppMessage() {
+    // 返回自定义分享信息
+    return {
+      title: 'My App',
+      desc: '江苏大地新闻',
+      path: 'pages/index/index',
+    };
+  },
+});
